b51dfe5a33bd090b4cd46d2d4e4083d2440a00d9
annaohkatsu <ancyincanada@gmail.com>
2024-11-28 22:07:46 -0800

    Added space

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ city = input("What's the name of the city you grew up in?\n")
 
 pets = input("What's your pet's name?\n")
 
-print('Your band name could be ' + city + pets)
+print('Your band name could be ' + city + " " + pets)
