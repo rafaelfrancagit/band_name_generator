5950f239cf5d8961d496cd4b83e027c4725b2d02
annaohkatsu <ancyincanada@gmail.com>
2024-11-28 21:51:57 -0800

    Initial commit

---
+++ task.py
@@ -0,0 +1,10 @@
+print('Welcome to the Band Name Generator!')
+
+city = input("What's the name of the city you grew up in?\n")
+
+pets = input("What's your pet's name?\n")
+
+print('Your band name could be ' + city + pets)
+
+
+fsdfsdfdsfsfd
