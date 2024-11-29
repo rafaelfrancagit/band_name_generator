888a8fead5f3115f146fd6077dada77c45af8c1b
Rafael Franca <139285405+rafaelfrancagit@users.noreply.github.com>
2024-11-28 22:13:22 -0800

    Update main.py
    -Assigned variable to band name
    -Asked for feedback from the user at the end of the code

--- main.py
+++ main.py
@@ -4,4 +4,11 @@ city = input("What's the name of the city you grew up in?\n")
 
 pets = input("What's your pet's name?\n")
 
-print('Your band name could be ' + city + " " + pets)
+band_name = city + ' ' + pets
+
+print(f'Your band name could be {band_name}')
+likeness = input('Did you like it? Y or N?').lower()
+if likeness == 'y':
+  print(f'The band {band_name} is awesome!!!'
+else:
+  print(f'Then you dont have a band name yet!')
